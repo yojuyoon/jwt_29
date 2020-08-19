b73924421634f29b827fa4b3e7a3c1784f78affc
Jun4928 <jun.choi.4928@gmail.com>
2020-08-19 10:29:32 +0900

    Add: Redux initial setting

--- src/index.js
+++ src/index.js
@@ -3,4 +3,16 @@ import ReactDOM from "react-dom";
 import Routes from "./Routes";
 import "./Styles/reset.scss";
 
-ReactDOM.render(<Routes />, document.getElementById("root"));
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import reducers from "./store/reducers";
+
+const store = createStore(reducers, composeWithDevTools);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Routes />
+  </Provider>,
+  document.getElementById("root")
+);
---
+++ src/store/actions/cartsActions.js
@@ -0,0 +1,77 @@
+class cartsActionsClass {
+  constructor() {
+    this.ADD_TO_CART = "ADD_TO_CART";
+    this.OVERRIDE_EXISTING_PRODUCT = "OVERRIDE_EXISTING_PRODUCT";
+    this.INCREMENT_QUANTITY = "INCREMENT_QUANTITY";
+    this.DECREMENT_QUANTITY = "DECREMENT_QUANTITY";
+    this.REMOVE_ONE_FROM_CART = "REMOVE_ONE_FROM_CART";
+    this.REMOVE_ALL_FROM_CART = "REMOVE_ALL_FROM_CART";
+    this.REMOVE_SELECTED_PRODUCTS_FROM_CART =
+      "REMOVE_SELECTED_PRODUCTS_FROM_CART";
+  }
+
+  addToCart = (product) => {
+    return {
+      type: this.ADD_TO_CART,
+      payload: {
+        product,
+      },
+    };
+  };
+
+  overrideExistingProduct = (product) => {
+    return {
+      type: this.OVERRIDE_EXISTING_PRODUCT,
+      payload: {
+        product,
+      },
+    };
+  };
+
+  incrementQuantity = (productId) => {
+    return {
+      type: this.INCREMENT_QUANTITY,
+      payload: {
+        id: productId,
+      },
+    };
+  };
+
+  decrementQuantity = (productId) => {
+    return {
+      type: this.DECREMENT_QUANTITY,
+      payload: {
+        id: productId,
+      },
+    };
+  };
+
+  removeOneFromCart = (productId) => {
+    return {
+      type: this.REMOVE_ONE_FROM_CART,
+      payload: {
+        id: productId,
+      },
+    };
+  };
+
+  removeAllFromCart = () => {
+    return {
+      type: this.REMOVE_ALL_FROM_CART,
+      payload: {
+        emptyCarts: [],
+      },
+    };
+  };
+
+  removeSelectedProductsFromCart = (productIdsArray) => {
+    return {
+      type: this.REMOVE_SELECTED_PRODUCTS_FROM_CART,
+      payload: {
+        productIdsArray,
+      },
+    };
+  };
+}
+
+export default cartsActionsClass;
